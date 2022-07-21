d5cfe9f4a1f79b5b6abc7fe1cfc3dfe21648e288
stephen498 <taiwostephen73@gmail.com>
2022-07-21 15:21:19 +0100

    commit

--- index1.js
+++ index1.js
@@ -57,35 +57,52 @@ function checkNetwork() {
             const img = document.createElement('img');
             img.src = "mtn.png";
             img.alt = "mtn everywhere you go";
-            img.style.width = '30px';
-            img.style.height = '30px';
-            document.getElementById('form').insertBefore(img, document.getElementById("phone"));
-            alert(`This is an ${network[value]} NUMBER. `);
+            img.style.width = '40px';
+            img.style.height = '40px';
+            img.style.paddingTop = '15px';
+            const p = document.createElement('p')
+            p.innerText = `This is an ${ network[value]} NUMBER `;
+
+            document.getElementById("form").insertAdjacentHTML('afterend', p.innerHTML);
+            document.body.append(img);
+
         } else if (network[value] == 'GLO') {
             const img = document.createElement('img');
             img.src = "glo.png";
             img.alt = "rule your world";
-            img.style.width = '30px';
-            img.style.height = '30px';
-            document.getElementById('form').insertBefore(img, document.getElementById("phone"));
-            alert(`This is a ${network[value]} NUMBER. `);
+            img.style.width = '40px';
+            img.style.height = '40px';
+            img.style.paddingTop = '15px';
+            const p = document.createElement('p')
+            p.innerText = `This is an ${ network[value]} NUMBER `;
+
+            document.getElementById("form").insertAdjacentHTML('afterend', p.innerHTML);
+            document.body.append(img);
 
         } else if (network[value] == 'AIRTEL') {
             const img = document.createElement('img');
             img.src = "airtel.png";
             img.alt = "The smartphone network";
-            img.style.width = '30px';
-            img.style.height = '30px';
-            document.getElementById('form').insertBefore(img, document.getElementById("phone"));
-            alert(`This is an ${network[value]} NUMBER. `);
+            img.style.width = '40px';
+            img.style.height = '40px';
+            img.style.paddingTop = '15px';
+            const p = document.createElement('p')
+            p.innerText = `This is an ${ network[value]} NUMBER `;
+
+            document.getElementById("form").insertAdjacentHTML('afterend', p.innerHTML);
+            document.body.append(img);
         } else if (network[value] == 'ETISALAT') {
             const img = document.createElement('img');
             img.src = "etisalat.png";
             img.alt = "9ja for life";
-            img.style.width = '30px';
-            img.style.height = '30px';
-            document.getElementById('form').insertBefore(img, document.getElementById("phone"));
-            alert(`This is an ${network[value]} NUMBER. `);
+            img.style.width = '40px';
+            img.style.height = '40px';
+            img.style.paddingTop = '15px';
+            const p = document.createElement('p')
+            p.innerText = `This is an ${ network[value]} NUMBER `;
+
+            document.getElementById("form").insertAdjacentHTML('afterend', p.innerHTML);
+            document.body.append(img);
         }
     }, 2000)
 
