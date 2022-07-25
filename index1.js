29cdf08026ff017b1be59e627bac203da33aec1c
stephen498 <taiwostephen73@gmail.com>
2022-07-25 15:40:42 +0100

    good

--- index1.js
+++ index1.js
@@ -82,52 +82,41 @@ function checkNetwork() {
             icon.innerText = `This is an ${ network[value]} NUMBER `;
             document.getElementById('icon').appendChild(img);
 
-
-
-        } else {
-
+        } else if (network[value] == 'GLO') {
+            const img = document.createElement('img');
+            img.src = "glo.png";
+            img.alt = "Rule your world";
+            img.style.width = '40px';
+            img.style.height = '40px';
+            img.style.paddingTop = '15px';
+            img.id = 'img';
             const icon = document.getElementById('icon')
             icon.innerText = `This is an ${ network[value]} NUMBER `;
+            document.getElementById('icon').appendChild(img);
 
+        } else if (network[value] == 'AIRTEL') {
+            const img = document.createElement('img');
+            img.src = "airtel.png";
+            img.alt = "Thew smartphone network";
+            img.style.width = '40px';
+            img.style.height = '40px';
+            img.style.paddingTop = '15px';
+            img.id = 'img';
+            const icon = document.getElementById('icon')
+            icon.innerText = `This is an ${ network[value]} NUMBER `;
+            document.getElementById('icon').appendChild(img);
+        } else if (network[value] == 'ETISALAT') {
+            const img = document.createElement('img');
+            img.src = "etisalat.png";
+            img.alt = "9ja for life";
+            img.style.width = '40px';
+            img.style.height = '40px';
+            img.style.paddingTop = '15px';
+            img.id = 'img';
+            const icon = document.getElementById('icon')
+            icon.innerText = `This is an ${ network[value]} NUMBER `;
+            document.getElementById('icon').appendChild(img);
         }
-        // } else if (network[value] == 'GLO') {
-        //     const img = document.createElement('img');
-        //     img.src = "glo.png";
-        //     img.alt = "rule your world";
-        //     img.style.width = '40px';
-        //     img.style.height = '40px';
-        //     img.style.paddingTop = '15px';
-        //     const p = document.createElement('p')
-        //     p.innerText = `This is a ${ network[value]} NUMBER `;
-
-        //     document.getElementById("form").insertAdjacentHTML('afterend', p.innerHTML);
-        //     document.body.append(img);
-
-        // } else if (network[value] == 'AIRTEL') {
-        //     const img = document.createElement('img');
-        //     img.src = "airtel.png";
-        //     img.alt = "The smartphone network";
-        //     img.style.width = '40px';
-        //     img.style.height = '40px';
-        //     img.style.paddingTop = '15px';
-        //     const p = document.createElement('p')
-        //     p.innerText = `This is an ${ network[value]} NUMBER `;
-
-        //     document.getElementById("form").insertAdjacentHTML('afterend', p.innerHTML);
-        //     document.body.append(img);
-        // } else if (network[value] == 'ETISALAT') {
-        //     const img = document.createElement('img');
-        //     img.src = "etisalat.png";
-        //     img.alt = "9ja for life";
-        //     img.style.width = '40px';
-        //     img.style.height = '40px';
-        //     img.style.paddingTop = '15px';
-        //     const p = document.createElement('p')
-        //     p.innerText = `This is an ${ network[value]} NUMBER `;
-
-        //     document.getElementById("form").insertAdjacentHTML('afterend', p.innerHTML);
-        //     document.body.append(img);
-        // }
     }
 };
 
