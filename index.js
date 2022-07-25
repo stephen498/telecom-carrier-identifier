222d02d25a4775e15eecb7d77fe99eb1ea45e960
stephen498 <taiwostephen73@gmail.com>
2022-07-25 15:24:19 +0100

    pattern format

---
+++ index.js
@@ -0,0 +1,3 @@
+import checkNetwork from './index1.js';
+
+document.addEventListener('DOMContentLoaded', checkNetwork);
--- index1.js
+++ index1.js
@@ -1,116 +1,140 @@
-function debounce(checkNetwork, timeout = 300) {
-    let timer;
+// function debounce(checkNetwork, timeout = 300) {
+//     let timer;
 
-    return (...arg) => {
-        clearTimeout(timer);
-        timer = setTimeout(() => { checkNetwork.apply(this, args); }, timeout);
+//     return (...arg) => {
+//         clearTimeout(timer);
+//         timer = setTimeout(() => { checkNetwork.apply(this, args); }, timeout);
 
-    };
-}
+//     };
+// }
 
+
+
+//     let prev = '';
+//     if (this.checkValidity()) {
+//         prev = this.value;
+//     } else {
+//         this.value = prev;
+//     }
+// };
 
 
 function checkNetwork() {
-    // const img = document.getElementById('img');
-    const phone = document.getElementById("phone");
-    console.log(phone.value);
-    const network = {
-        '0810': 'MTN',
-        '0803': 'MTN',
-        '0703': 'MTN',
-        '0903': 'MTN',
-        '0806': 'MTN',
-        '0706': 'MTN',
-        '0813': 'MTN',
-        '0814': 'MTN',
-        '0816': 'MTN',
-        '0805': 'GLO',
-        '0705': 'GLO',
-        '0905': 'GLO',
-        '0807': 'GLO',
-        '0815': 'GLO',
-        '0811': 'GLO',
-        '0809': 'ETISALAT',
-        '0909': 'ETISALAT',
-        '0817': 'ETISALAT',
-        '0818': 'ETISALAT',
-        '0802': 'AIRTEL',
-        '0902': 'AIRTEL',
-        '0701': 'AIRTEL',
-        '0808': 'AIRTEL',
-        '0708': 'AIRTEL',
-        '0812': 'AIRTEL',
-        '0904': 'AIRTEL'
-    }
+    const val = document.getElementById('val');
+    val.onclick = function(e) {
+        e.preventDefault();
 
-    let value = '';
+        const phone = document.getElementById("phone");
+        if (String(phone.value).length < 7) {
 
-    value = String(phone.value).slice(0, 4);
+            const icon = document.getElementById('icon')
+            icon.innerText = `iNVALID FORMAT NUMBER `;
+            return;
+        }
+        console.log(phone.value);
 
-    setTimeout(() => {
+        const network = {
+            '+234810': 'MTN',
+            '+234803': 'MTN',
+            '+234703': 'MTN',
+            '+234903': 'MTN',
+            '+234806': 'MTN',
+            '+234706': 'MTN',
+            '+234813': 'MTN',
+            '+234814': 'MTN',
+            '+234816': 'MTN',
+            '+234805': 'GLO',
+            '+234705': 'GLO',
+            '+234905': 'GLO',
+            '+234807': 'GLO',
+            '+234815': 'GLO',
+            '+234811': 'GLO',
+            '+234809': 'ETISALAT',
+            '0909': 'ETISALAT',
+            '0817': 'ETISALAT',
+            '0818': 'ETISALAT',
+            '0802': 'AIRTEL',
+            '0902': 'AIRTEL',
+            '0701': 'AIRTEL',
+            '0808': 'AIRTEL',
+            '0708': 'AIRTEL',
+            '0812': 'AIRTEL',
+            '0904': 'AIRTEL'
+        }
+
+
+
+        let value = '';
+
+        value = String(phone.value).slice(0, 7);
+
+
         if (network[value] == 'MTN') {
-            // phone.insertAdjacentHTML('afterend',
-            //     '<img src = "mtn.png" alt = "mtn everywhere you go" / > ')
-            // const img = document.getElementsByTagName('img');
-            // img.style.width = 2;
-            // img.style.height = 2;
+
             const img = document.createElement('img');
             img.src = "mtn.png";
             img.alt = "mtn everywhere you go";
             img.style.width = '40px';
             img.style.height = '40px';
             img.style.paddingTop = '15px';
-            const p = document.createElement('p')
-            p.innerText = `This is an ${ network[value]} NUMBER `;
+            img.id = 'img';
+            const icon = document.getElementById('icon')
+            icon.innerText = `This is an ${ network[value]} NUMBER `;
+            document.getElementById('icon').appendChild(img);
 
-            document.getElementById("form").insertAdjacentHTML('afterend', p.innerHTML);
-            document.body.append(img);
 
-        } else if (network[value] == 'GLO') {
-            const img = document.createElement('img');
-            img.src = "glo.png";
-            img.alt = "rule your world";
-            img.style.width = '40px';
-            img.style.height = '40px';
-            img.style.paddingTop = '15px';
-            const p = document.createElement('p')
-            p.innerText = `This is a ${ network[value]} NUMBER `;
 
-            document.getElementById("form").insertAdjacentHTML('afterend', p.innerHTML);
-            document.body.append(img);
+        } else {
 
-        } else if (network[value] == 'AIRTEL') {
-            const img = document.createElement('img');
-            img.src = "airtel.png";
-            img.alt = "The smartphone network";
-            img.style.width = '40px';
-            img.style.height = '40px';
-            img.style.paddingTop = '15px';
-            const p = document.createElement('p')
-            p.innerText = `This is an ${ network[value]} NUMBER `;
+            const icon = document.getElementById('icon')
+            icon.innerText = `This is an ${ network[value]} NUMBER `;
 
-            document.getElementById("form").insertAdjacentHTML('afterend', p.innerHTML);
-            document.body.append(img);
-        } else if (network[value] == 'ETISALAT') {
-            const img = document.createElement('img');
-            img.src = "etisalat.png";
-            img.alt = "9ja for life";
-            img.style.width = '40px';
-            img.style.height = '40px';
-            img.style.paddingTop = '15px';
-            const p = document.createElement('p')
-            p.innerText = `This is an ${ network[value]} NUMBER `;
-
-            document.getElementById("form").insertAdjacentHTML('afterend', p.innerHTML);
-            document.body.append(img);
         }
-    }, 2000)
+        // } else if (network[value] == 'GLO') {
+        //     const img = document.createElement('img');
+        //     img.src = "glo.png";
+        //     img.alt = "rule your world";
+        //     img.style.width = '40px';
+        //     img.style.height = '40px';
+        //     img.style.paddingTop = '15px';
+        //     const p = document.createElement('p')
+        //     p.innerText = `This is a ${ network[value]} NUMBER `;
 
-}
+        //     document.getElementById("form").insertAdjacentHTML('afterend', p.innerHTML);
+        //     document.body.append(img);
 
+        // } else if (network[value] == 'AIRTEL') {
+        //     const img = document.createElement('img');
+        //     img.src = "airtel.png";
+        //     img.alt = "The smartphone network";
+        //     img.style.width = '40px';
+        //     img.style.height = '40px';
+        //     img.style.paddingTop = '15px';
+        //     const p = document.createElement('p')
+        //     p.innerText = `This is an ${ network[value]} NUMBER `;
 
-const reloadtButton = document.querySelector("#reload");
-// Reload everything:
-function reload() {
-    reload = location.reload();
-}
+        //     document.getElementById("form").insertAdjacentHTML('afterend', p.innerHTML);
+        //     document.body.append(img);
+        // } else if (network[value] == 'ETISALAT') {
+        //     const img = document.createElement('img');
+        //     img.src = "etisalat.png";
+        //     img.alt = "9ja for life";
+        //     img.style.width = '40px';
+        //     img.style.height = '40px';
+        //     img.style.paddingTop = '15px';
+        //     const p = document.createElement('p')
+        //     p.innerText = `This is an ${ network[value]} NUMBER `;
+
+        //     document.getElementById("form").insertAdjacentHTML('afterend', p.innerHTML);
+        //     document.body.append(img);
+        // }
+    }
+};
+
+export default checkNetwork;
+
+// const reloadtButton = document.querySelector("#reload");
+// // Reload everything:
+// function reload() {
+//     reload = location.reload();
+// }
